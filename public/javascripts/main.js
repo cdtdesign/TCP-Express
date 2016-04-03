0722b26af2d1ef6675426dc658f7d95e04a5d7d7
Christina Thorpe <cdtdesign@fullsail.edu>
2016-04-02 20:19:32 -0400

    Create Download page

---
+++ public/javascripts/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function ($) {
+
+	$('.my-slider').unslider();
+
+	// hide .navbar first
+	$('.navwht').hide();
+
+	// bounce in .navwht
+	$(function () {
+			$(window).scroll(function () {
+
+					var animationName = 'animated bounceInDown';
+
+					// set distance user needs to scroll before we start fadeIn
+					if ($(window).scrollTop() > 400) {
+							$('.navbar').fadeOut();
+							$('.navwht').show();
+							$('.navwht').fadeIn();
+							$('.navwht').addClass(animationName);
+					} else {
+							$('.navwht').removeClass(animationName);
+							$('.navwht').fadeOut();
+							$('.navbar').fadeIn();
+					}
+			});
+	});
+});
