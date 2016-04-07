5fa3ffe4b939f715974b0920c72553f36c5367be
Christina Thorpe <cdtdesign@fullsail.edu>
2016-04-07 12:15:57 -0400

    Edited routes to get route to blog.html working to show begining layout of Journey Blog page

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET blog page. */
-router.get('/journeyblog', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('blog', {  });
 });
 
