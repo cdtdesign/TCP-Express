a89cd13de26da3696edd458b3c7f5792f1e99a7a
Christina Thorpe <cdtdesign@fullsail.edu>
2016-04-06 20:11:19 -0400

    Edits to contact.html, blog.html & term.html

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET blog page. */
-router.get('/', function(req, res, next) {
+router.get('/journey-blog', function(req, res, next) {
   res.render('blog', { title: 'Express' });
 });
 
