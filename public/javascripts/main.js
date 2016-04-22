d35ef6c37b3733e6338cf9c8a74cd24dacf54ab6
Christina Thorpe <cdtdesign@fullsail.edu>
2016-04-21 20:32:34 -0400

    Edits to CSS & blog.html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
 $('.grid').masonry({
   // options
   itemSelector: '.grid-item',
-  columnWidth: 300
+  columnWidth: 275
 });
 
 
--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../database');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'tcp',
+  password : process.env.TCP_DATABASE_PASSWORD,
+  database : 'Passport'
+});
+
+// connection.connect();
 
 /* GET blog page. */
 router.get('/', function(req, res, next) {
-  res.render('blog', {  });
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+  });
+
+  var queryString = 'SELECT * FROM journeys JOIN travelers ON travelers.id = journeys.traveler';
+  connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+    console.log("Finished the query");
+
+    res.render('blog', {
+      "journeys": rows
+    });
+  });
+  // connection.end();
 });
 
 module.exports = router;
