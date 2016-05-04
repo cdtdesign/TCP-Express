87f062c1aa25ce0824801a9762996083a8c787a6
Christina Thorpe <cdtdesign@fullsail.edu>
2016-05-03 20:30:18 -0400

    Debugging

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+
 // Bootstrap Middleware
 app.use(require('morgan')('combined'));
 app.use(require('serve-static')(__dirname + '/../../public'));
@@ -39,6 +40,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 var mongoose = require('mongoose');
+var mongoose_db = mongoose.connection;
 mongoose.connect('mongodb://localhost/passport');
 
 var userSchema = mongoose.Schema({
@@ -63,47 +65,76 @@ var connection = mysql.createConnection({
   database : 'Passport'
 });
 
-// Oauth2 Authentication Strategy
-passport.use(new OAuth2Strategy({
-    authorizationURL: 'http://beta-express.travelingchildrenproject.com/oauth2/authorize',
-    tokenURL: 'http://beta-express.travelingchildrenproject.com/oauth2/token',
-    clientID: process.env.FACEBOOK_KEY,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "http://beta-express.travelingchildrenproject.com/auth/oauth2/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    db.users.findOrCreate({ oauth2Id: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
 
-// Facebook Authentication Strategy
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_KEY,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "http://beta-express.travelingchildrenproject.com/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    db.users.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+mongoose_db.on('error', function (err) {
+  if (err) throw err;
+});
 
-// Twitter Authentication Strategy
-passport.use(new TwitterStrategy({
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://beta-express.travelingchildrenproject.com/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, cb) {
-    db.users.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      return cb(err, user);
+mongoose_db.once('open', function() {
+  passport.use(new Strategy(
+    function(username, password, cb) {
+      db.users.findByUsername(username, function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+      });
+    }));
+
+  // Oauth2 Authentication Strategy
+  passport.use(new OAuth2Strategy({
+      authorizationURL: 'http://beta-express.travelingchildrenproject.com/oauth2/authorize',
+      tokenURL: 'http://beta-express.travelingchildrenproject.com/oauth2/token',
+      clientID: process.env.FACEBOOK_KEY,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "http://beta-express.travelingchildrenproject.com/auth/oauth2/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      db.users.findOrCreate({ oauth2Id: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  ));
+
+  // Facebook Authentication Strategy
+  passport.use(new FacebookStrategy({
+      clientID: process.env.FACEBOOK_KEY,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "http://beta-express.travelingchildrenproject.com/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      db.users.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  ));
+
+  // Twitter Authentication Strategy
+  passport.use(new TwitterStrategy({
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      callbackURL: "http://beta-express.travelingchildrenproject.com/auth/twitter/callback"
+    },
+    function(token, tokenSecret, profile, cb) {
+      db.users.findOrCreate({ twitterId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  ));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+      if (err) { return cb(err); }
+      cb(null, user);
     });
-  }
-));
+  });
+
+});
 
 app.get('/auth/twitter',
   passport.authenticate('twitter'));
@@ -127,16 +158,7 @@ app.get('/auth/twitter/callback',
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
 
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
 
 // =======
 // >>>>>>> 55d8ed922b1cbed00e06bccdb8ea4d3aab1a1cae
@@ -194,23 +216,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-// Passport Config --- SIGNIN
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
 
 module.exports = app;
