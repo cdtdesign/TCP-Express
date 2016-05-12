cb9b78423e1629e3b4efae59d8dec9c0fa2879ea
Christina Thorpe <cdtdesign@fullsail.edu>
2016-05-12 12:37:23 -0400

    Edit to main.js - Masonry

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 $(window).on('load resize', function() {
 	return $('.grid').masonry({
 		itemSelector: '.grid-post',
-		columnWidth: 300
+		columnWidth: 275
 	});
 
 });
