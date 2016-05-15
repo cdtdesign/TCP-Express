9566220893d821738bd8c052cc56a837cd3f9d4d
Christina Thorpe <cdtdesign@fullsail.edu>
2016-05-14 22:07:10 -0400

    Removing MySQL

--- models/user.js
+++ models/user.js
@@ -1,13 +1,3 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'tcp',
-  password : process.env.TCP_DATABASE_PASSWORD,
-  database : 'Passport'
-});
-
-connection.connect();
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -19,14 +9,3 @@ var UserSchema = new Schema({
 });
 
 var User = mongoose.model('User', UserSchema);
-
-var records;
-connection.query('SELECT * FROM travelers', function (err, returned_records, fields) {
-  if (err) throw err;
-  records = returned_records;
-  return records;
-});
-
-// connection.query('INSERT INTO travelers (email, password, first_name, last_name, traveler_name, traveler_gender, parent_gender) VALUES ("' + req.body.email + '", "' + req.body.password + '", "' + req.body.first_name + '", "' + req.body.last_name + '", "' + req.body.traveler_name + '", 3, 3)');
-
-connection.end();
--- routes/blog.js
+++ routes/blog.js
@@ -1,39 +1,11 @@
 var express = require('express');
 var router = express.Router();
-// var database = require('../database');
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'tcp',
-  password : process.env.TCP_DATABASE_PASSWORD,
-  database : 'Passport'
-});
-
-connection.connect();
 
 /* GET blog page. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
+  res.render('blog', {
+    "journeys": rows
   });
-
-  new Promise (function (fulfill, reject) {
-    var queryString = 'SELECT * FROM journeys JOIN travelers ON travelers.id = journeys.traveler';
-    connection.query(queryString, function(err, rows, fields) {
-      if (err) throw err;
-      console.log("Finished the query");
-
-      res.render('blog', {
-        "journeys": rows
-      });
-
-    });
-
-  }).then (function () {
-      connection.end();
-  });
-
-  });
+});
 
 module.exports = router;
