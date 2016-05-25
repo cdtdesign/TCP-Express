2a9bda5d1dc99e6cdfef72dbc1b5e1dc38aa2f04
Christina Thorpe <cdtdesign@fullsail.edu>
2016-05-25 01:51:32 -0400

    Edited travelerPic for FB & Twitter in navbar.html + edit to user.js with appRootPath, lines 3 & 9

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var appRootPath = require('app-root-path');
 
 var userSchema = new Schema({
   username				: String,
 	name				: String,
 	provider_id : {type: String},
-	photo			 : String,
+	photo			 : appRootPath + String,
   password : String,
   first_name : String,
   last_name : String,
