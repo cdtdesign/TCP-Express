d8638eb013080e494a8e026e9fb11c79ac2117df
Christina Thorpe <cdtdesign@fullsail.edu>
2016-06-10 19:53:08 -0400

    Text edits to sponsorship.html + CSS edit to spans

--- routes/mypassport.js
+++ routes/mypassport.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require ('../models/user');
+var swig = require('swig');
 
 /* GET mypassport page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,10 @@ router.get('/', function(req, res, next) {
 
 /* GET editpassport page. */
 router.get('/edit', function(req, res, next) {
+  swig.setFilter('dateFormat', function (input) {
+    var date = new Date(input);
+    return ("0" + date.getFullYear()).slice(-4) + '-' + ("0" + date.getMonth()).slice(-2) + '-' + ("0" + date.getDate()).slice(-2);
+  });
   res.render('editpassport');
 });
 
