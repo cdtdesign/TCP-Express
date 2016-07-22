0b506b6dde094ff61befd8b6b00dd2596e858cdd
Christina Thorpe <cdtdesign@fullsail.edu>
2016-07-21 22:46:00 -0400

    Facebook socal share in Journey Blog

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -10,15 +10,15 @@ $(document).ready(function () {
     return FB.getLoginStatus(function(response) {
       return $('.share-with-facebook').click(function() {
         var creator, description, header_image, journey_uuid, title;
-        journey_uuid = $(this).parents('.journeyPost').data('journey-uuid');
-        title = $(this).parents('.journeyPost').children(':first-of-type').children('.jp_title').children('span').text();
-        creator = $(this).parents('.journeyPost').children(':last-of-type').children('.jp_fname_date').children('em').children('a').text().trim();
-        header_image = $(this).parents('.journeyPost').children('.jp_img').children('img').attr('src');
-        description = $(this).parents('.journeyPost').children(':last-of-type').children('.jp_body').text().trim();
+        journey_uuid = $(this).parents('.grid-post').data('journey-uuid');
+        title = $(this).parents('.grid-post').children('.blogTitle').text();
+        creator = $(this).parents('.grid-post').children(':last-of-type').children('.jp_fname_date').children('em').children('a').text().trim();
+        header_image = $(this).parents('.grid-post').children('.jp_img').children('img').attr('src');
+        description = $(this).parents('.grid-post').children(':last-of-type').children('.jp_body').text().trim();
         return FB.ui({
           method: 'feed',
           link: 'travelingchildrenproject.com/journeys#' + journey_uuid,
-          name: creator + '\'s Journey to ' + title,
+          name: title,
           description: description,
           picture: 'http://travelingchildrenproject.com' + header_image
         });
