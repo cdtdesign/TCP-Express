59afbb39f79319e5379229e204faa3d3971d4d19
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-04-19 19:41:11 -0400

    Create a Gulp task for development (with Browser-Sync)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 var nodemon = require('gulp-nodemon');
-// var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function () {
   // Run Mongo
@@ -9,14 +9,12 @@ gulp.task('default', function () {
 
   // Run Nodemon
   nodemon();
+});
 
+gulp.task('dev', ['default'], function () {
   // Run Browser-Sync
-  // browserSync.init([
-  //   "views/***",
-  //   "public/stylesheets/***"
-  // ], {
-  //   proxy: 'localhost:9000',
-  //   open: true,
-  //   browser: "Google Chrome"
-  // });
+  browserSync.init(["views/***", "public/***"], {
+    proxy: 'localhost:9000',
+    browser: "Google Chrome"
+  });
 });
