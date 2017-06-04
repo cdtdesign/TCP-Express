e43f69e109763b058840bda9ee8a9d4405bdf7a1
Alexander Rhett Crammer <alexander2475914@gmail.com>
2017-06-04 17:07:53 -0400

    Use HTTPS to share Journey Posts via Facebook instead of HTTP

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   // Facebook Share
-  $.getScript('http://connect.facebook.net/en_US/sdk.js', function() {
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function() {
     FB.init({
       appId: '1660831194160373',
       version: 'v2.5'
