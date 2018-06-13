eda7b4d18e88693b24d31a58a6ad910dbe117c6b
Alexander Rhett Crammer <Alexander2475914@gmail.com>
2018-06-13 12:15:54 -0400

    Fix authentication once and for all

--- routes/API.js
+++ routes/API.js
@@ -11,12 +11,38 @@ router.post('/iOS/auth/signin', function (req, res, next) {
   req.isAPI = true;
 
   passport.authenticate('local', function (err, user, info) {
-    console.log(err, user, info);
-  });
+    if (err) {
+      return res.json({
+        "success": false,
+        "error": err
+      });
+    } else {
+      req.logIn(user, function (err) {
+        if (err) return next(err);
+
+        return res.json({
+          "user": {
+            "id": user._id || '',
+            "first_name": user.first_name || '',
+            "last_name": user.last_name || '',
+            "email": user.email || '',
+            "password": user.password || '',
+            "parent_gender": user.parent_gender || '',
+            "address": {
+              "phone": user.address.phone || '',
+              "street": user.address.street || '',
+              "city": user.address.city || '',
+              "state": user.address.state || '',
+              "zip": user.address.zip || ''
+            }
+          }
+        });
+      });
+    }
+  })(req, res, next);
 });
 
 router.post('/iOS/auth/signup', function (req, res, next) {
-  console.log(".post('/iOS/auth/signup'");
   req.isAPI = true;
 
   passport.authenticate('local', function (err, user, info) {
@@ -37,23 +63,6 @@ router.post('/iOS/auth/signup', function (req, res, next) {
             if (err) throw err;
 
             User.findOne({'username': 'Alexander2475914@gmail.com'}, 'password', function (err, password) {
-              console.log({
-                "user": {
-                  "id": user._id || '',
-                  "first_name": user.first_name || '',
-                  "last_name": user.last_name || '',
-                  "email": user.email || '',
-                  "password": user.password || '',
-                  "parent_gender": user.parent_gender || '',
-                  "address": {
-                    "phone": user.address.phone || '',
-                    "street": user.address.street || '',
-                    "city": user.address.city || '',
-                    "state": user.address.state || '',
-                    "zip": user.address.zip || ''
-                  }
-                }
-              });
               return res.json({
                 "user": {
                   "id": user._id || '',
@@ -81,25 +90,11 @@ router.post('/iOS/auth/signup', function (req, res, next) {
         }
       })(req, res, next);
     }
-
-    // req.logIn(user, function (err) {
-    //   if (err) return next(err);
-    //
-    //   return res.json({
-    //     "success": true,
-    //     "user": {
-    //       "id": user._id,
-    //       "email": user.email,
-    //       "firstName": user.first_name,
-    //       "lastName": user.last_name
-    //     }
-    //   });
-    // });
   })(req, res, next);
 });
 
 // Journeys
-router.post('/journeys', function (req, res) {
+router.post('/iOS/journeys', function (req, res) {
   // Send over some journeys from the database
   Journey
     .find(function (err, journeys) {
